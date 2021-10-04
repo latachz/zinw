6e12d81134fae0de52cda2a8300d801f7eb3d117
Kacper <kacpx123@gmail.com>
2021-10-04 21:32:24 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Barcode />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Barcode from "./components/Barcode.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Barcode,
   },
 };
 </script>
---
+++ src/components/Barcode.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <StreamBarcodeReader
+      @decode="onDecode"
+      @loaded="onLoaded"
+    ></StreamBarcodeReader>
+  </div>
+</template>
+
+<script>
+import { StreamBarcodeReader } from "vue-barcode-reader";
+
+export default {
+  name: "Barcode",
+  components: { StreamBarcodeReader },
+  methods: {
+    onDecode: function (a) {
+      console.log(a);
+    },
+    onLoaded: function (a) {
+      console.log(a);
+    },
+  },
+};
+</script>
