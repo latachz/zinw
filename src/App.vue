23de164c086da0844da5e8027b49e32e2a4b9736
Kacper Latuszewski <kacper.latuszewski@appunite.com>
2021-10-05 08:17:31 +0200

    reinit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <Barcode />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
-
-<script>
-import Barcode from "./components/Barcode.vue";
-
-export default {
-  name: "App",
-  components: {
-    Barcode,
-  },
-};
-</script>
 
 <style lang="scss">
 #app {
@@ -23,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
